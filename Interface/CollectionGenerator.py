cb08d209720843c2de31a61fea363a39f1af0d9e
Chase-Warwick <49159449+Chase-Warwick@users.noreply.github.com>
2020-11-18 14:41:33 -0700

    Collapsed FileManager into CollectionGenerator

--- Interface/CollectionGenerator.py
+++ Interface/CollectionGenerator.py
@@ -36,7 +36,30 @@ class CollectionGenerator:
 			batchInsert.append(row)
 		
 		collection.insert_many(batchInsert)
-			
+		
+import os.path
+import json
+class FileManager:
+	READ_CONSTANT = "r"	
+	
+	def readJsonFile(fileName):
+		directory = os.path.dirname(__file__) + "/../../" + fileName		#Directory where file is contained
+
+		if os.path.splitext(directory)[1] != ".json":				#Raise error if file is of wrong type
+			raise ValueError("Wrong File Type")
+
+		managedFile = open(directory, FileManager.READ_CONSTANT)		#Open file or throw exception if file does not exist
+		fileContent = json.loads(managedFile.read())					#
+		return fileContent
+
+if __name__ == "__main__":
+	try:
+		invalid = FileManager.readJsonFile("Tag.txt")
+	except ValueError as e:
+		print(e)
+	jsonDict = FileManager.readJsonFile("Tags.json")
+	for row in jsonDict["tags"]["row"]:
+		print(row)
 
 if __name__ == "__main__":
 	c = CollectionGenerator
