38cac6124c05811e5b17085ef70a511a8580b012
Chase-Warwick <warwick@ualberta.ca>
2020-11-19 00:03:40 -0700

    Moved Menu.py into Interface Package

--- UI/SearchForPostsScreen.py
+++ UI/SearchForPostsScreen.py
@@ -1,7 +1,8 @@
 import pymongo
 from Interface.Terminal import Terminal
-
+from Interface.Menu import Menu
 class SearchForQuestionsScreen:
+	menu = Menu()
 	"""
 	A screen which handles searching for posts and the processes
 	associated with it.
@@ -38,7 +39,11 @@ class SearchForQuestionsScreen:
 		searchKeys = searchForQuestionsScreen.getParsedKeywords()
 		return SearchForQuestions.getQuestions(searchKeys)
 
-	def printMenuScreen():
+	def printMenuScreen(posts):
+		for post in posts:
+			if post is not None:
+				menuOption = post[key]['Title'] + "---" + post[key]['CreationDate'] + "---" + post[key]['Score'] + "---" + post[key]['AnswerCount']
+				SearchForQuestionsScreen.menu.addItem(menuOption)
 
 class SearchForQuestions:
 	client = pymongo.MongoClient('localhost', int(Terminal.getPort()))
