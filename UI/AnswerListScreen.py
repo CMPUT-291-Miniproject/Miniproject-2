749e1ba87129358f01ea093d34cada9f11a52a9a
Chase-Warwick <warwick@ualberta.ca>
2020-11-20 10:00:50 -0700

    typo

--- UI/AnswerListScreen.py
+++ UI/AnswerListScreen.py
@@ -9,7 +9,7 @@ class AnswerListScreen:
 	def printScreen(question):
 		answers = AnswerList.getAnswers(question)
 		menu = AnswerListMenu(answers, question)
-		menu.printAnswer()
+		menu.printMenu()
 
 
 
