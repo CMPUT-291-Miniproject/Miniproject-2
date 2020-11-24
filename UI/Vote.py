1e265e82d95b863270c250b25ba858a430b5177a
Chase-Warwick <warwick@ualberta.ca>
2020-11-24 13:45:53 -0700

    Improving performance

--- UI/Vote.py
+++ UI/Vote.py
@@ -71,8 +71,7 @@ class Vote:
 			A String object representing a unique ID
 		"""
 		results = collection.find_one({}).sort([("Id", 1),]
-		for result in results:
-			print(result)
+		print(results)
 
 
 	def userVoted(collection, post, userID):
