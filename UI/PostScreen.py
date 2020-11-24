9032591ebfcdc139d248b1d12f64bb0049ed12ec
Cameron Matthew <cameron.matthew101@gmail.com>
2020-11-23 18:22:39 -0700

    Quick push for a refresh. Answering questions now works too, just need to add voting and listing answers; after that we're good.

--- UI/PostScreen.py
+++ UI/PostScreen.py
@@ -126,8 +126,8 @@ class PostScreen:
 		
 		#Main input loop, runs until user input is valid, or they quit out of the menue.
 		while repeat:
-			self.__terminal__.printCenter("---POSTING ANSWER---")
-			self.__terminal__.printCenter("To go back without posting a question, input BACK during any prompt.")
+			Terminal.printCenter("---POSTING ANSWER---")
+			Terminal.printCenter("To go back without posting a question, input BACK during any prompt.")
 			print('\n')
 			
 			#gets the title and body of the post in a tuple
@@ -138,24 +138,15 @@ class PostScreen:
 			print(info[1])
 			print('\n')
 			
-					
-			#get title of post
-			title = input("Please enter the title of the answer: ")
-			if title.lower().strip() == "back":
-				return
-			
-			print('\n')
-			
 			#Get body of post
-			body = input("Please enter the description of the answer: ")
+			body = input("Please enter your answer: ")
 			if body.lower().strip() == "back":
 				return
 				
 			#input validation loop. breaks if input is Y or N.
 			while True:
-				self.__terminal__.clear()
-				self.__terminal__.printCenter(title)
-				self.__terminal__.printCenter(body)
+				Terminal.clear()
+				Terminal.printCenter(body)
 				print("\n")
 				
 			
@@ -174,7 +165,7 @@ class PostScreen:
 					input("Invalid input. Press enter to continue.")
 				
 			#adds the answer to the database and alerts the user that the operation was a success.
-			self.__body__.add_post(title, body, pid)
+			self.__body__.add_post(None, body, None, pid)
 			input("Your answer has been posted. Press enter to return back to the main menue.")
 			return
 		
