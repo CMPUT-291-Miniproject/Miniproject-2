dfee60b118f39d6034fe4626425ad311141a966d
Chase-Warwick <warwick@ualberta.ca>
2020-11-24 14:16:44 -0700

    Improving performance

--- UI/Vote.py
+++ UI/Vote.py
@@ -70,7 +70,7 @@ class Vote:
 		Returns:
 			A String object representing a unique ID
 		"""
-		results = collection.find_one(sort=[("Id", pymongo.ASCENDING)])
+		results = collection.find_one(sort=[("Id", pymongo.DESCENDING)])
 		print(results)
 
 
